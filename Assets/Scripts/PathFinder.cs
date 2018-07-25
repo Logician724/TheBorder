875221a58cceb15fcb06b53d61dd6e565b21e10e
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-07-25 23:58:00 +0200

    feat: made a basic chase scene

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinder : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    public GameObject refugee;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        agent.SetDestination(refugee.transform.position);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Hi");
+        }
+    }
+}
---
+++ Assets/Scripts/TestTerrain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestTerrain : MonoBehaviour {
+
+    public GameObject cylinder;
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            cylinder.transform.position -= new Vector3(0.5f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            cylinder.transform.position += new Vector3(0.5f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            cylinder.transform.position -= new Vector3(0, 0, 0.5f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            cylinder.transform.position += new Vector3(0, 0, 0.5f);
+        }
+        
+    }
+}
